f05cfe1cbdaafeb7c62b76a8483f570b50874b78
Charles Ueltschey <me@cueltschey.com>
2026-01-13 16:46:38 -0600

    Remove unnecessary GPU test

--- worker_thread.py
+++ worker_thread.py
@@ -3,9 +3,8 @@ import logging
 import secrets
 import sys
 import os
-from docker.types import DeviceRequest
 
-class llm_worker(WorkerThread):
+class RtTestComponent(WorkerThread):
     def __init__(self, influxdb_client, docker_client, process_config):
         super().__init__(influxdb_client, docker_client, process_config)
         self.access_token = secrets.token_urlsafe(32)
@@ -22,15 +21,6 @@ class llm_worker(WorkerThread):
 
         self.config.container_volumes[self.config.config_file] = {"bind": "/test.yaml", "mode": "ro"}
         self.setup_volumes()
-
-        if self.config.process_config.get("enable_gpu", False):
-            self.config.device_requests.append(
-                DeviceRequest(
-                    count=-1,
-                    capabilities=[["gpu"]],
-                    driver="nvidia"
-                )
-            )
 
         self.start_container()
 
